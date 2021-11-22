be476637c87b7d646e206b0d62c1c6309176b3de
ehtisham57 <81214666+ehtisham57@users.noreply.github.com>
2021-11-22 10:00:28 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,113 @@
+//*****************************Chapter 38 to 42
+//============================FUNCTIONS
+
+//Question 1
+
+// function greet() {
+//     var a = +prompt("Enter a value")
+//     var b = +prompt("Required power")
+//     document.write(Math.pow(a, b))
+// }
+// greet()
+
+
+//Question 2
+
+
+// function greet() {
+//     var year = +prompt("Enter Your Year")
+//     if ((year % 100 != 0) && (year % 4 == 0) || (year % 400 == 0)) {
+//         alert("Leap Year")
+//     } else { alert("Not Leap Year") }
+// }
+// greet()
+
+//Question 3
+
+
+// var a = +prompt("Enter value 1")
+// var b = +prompt("Enter value 2")
+// var c = +prompt("Enter value 3")
+
+
+// function greet() {
+//     var s = (a + b + c) / 2
+
+
+//     var area = s * ((s - a) * (s - b) * (s - c))
+
+//     document.write("Area Of a Triangle: " + area)
+
+// }
+// greet()
+
+
+
+//*=============================================EVENTS
+
+//Question 1
+
+// function dil() {
+//     alert("Hello Classss")
+// }
+
+
+
+//Question 2
+
+// function sell() {
+//     alert("Thanks For Buy")
+// }
+
+//Question 4
+
+// function bulbToggler(id, e) {
+//     if (id === 1) {
+//         e.src = "./images/1.gif"
+//     } else { e.src = "./images/2.gif" }
+// }
+
+//Question 5
+
+
+// function incVal() {
+//     var incre = document.getElementById('incrdecr')
+//     var value = incre.innerHTML;
+
+//     ++value
+//     console.log(value)
+//     document.getElementById('incrdecr').innerHTML = value
+// }
+
+// function decVal() {
+//     var decre = document.getElementById('incrdecr')
+//     var value = decre.innerHTML;
+
+//     --value
+//     console.log(value)
+//     document.getElementById('incrdecr').innerHTML = value
+// }
+
+
+//=========================EVENTS
+//chapter 49 to 52
+
+
+//Question 1
+
+
+///Question 2
+
+
+
+// console.log(a)
+
+
+// function seemore() {
+//     var b = document.getElementById('text')
+//         // console.log(b)
+//     var a = "Vitae maiores cum in fugiat ? Dolore,temporibus labore neque voluptate pariatur molestias sed provident natus nisi laborum.Doloremque suscipit dolores voluptatum, odio dicta voluptatem libero, iste cumque esse excepturi ab repellat debitisquaerat ut laudantium accusantium voluptates molestias quia quo nemo, ullam beatae non ipsa modi!Itaque ducimus a, veritatis cupiditate blanditiis iure tempore ut eum nihil aspernatur incidunt rem iusto ? In reprehenderit numquam ipsam repellatex ? "
+//         // console.log(a) 
+//     var result = b.innerHTML + a
+//     document.write(result)
+// }
